889c02de904e5657f4bfb2ee689cd3f3a48a294a
rambaldeo <77577818+rambaldeo@users.noreply.github.com>
2024-04-18 17:55:54 -0400

    Home Page Changes

--- Components/Home.js
+++ Components/Home.js
@@ -1,64 +1,12 @@
-import React from 'react';
-import { View, StyleSheet, StatusBar } from 'react-native'; // Removed unused imports
-import SquareWithImageBackground from './SquareWithImageBackground'; // Fixed import path
+import React, { useState } from 'react';
+import { View, StyleSheet, StatusBar, TouchableOpacity, Modal, Text } from 'react-native';
+import MenuDropDown from './MenuDropDown';
 
-const Home = ({ navigation }) => {
-    const currentTime = new Date();
-    const currentHour = currentTime.getHours();
-    const name = 'Caretaker';
-
+const Home = () => {
+    <StatusBar style="auto" />
     return (
         <View style={styles.container}>
-            <View style={styles.row}>
-                <SquareWithImageBackground
-                    id="AboutMe"
-                    title="About Me"
-                    imageUrl={require('../assets/splash.png')}
-                    color="#E5B9FA"
-                    fontSize={45}
-                />
-                <SquareWithImageBackground
-                    id="Education"
-                    title="Education"
-                    imageUrl={require('../assets/splash.png')}
-                    color="#E5B9FA"
-                    fontSize={45}
-                />
-            </View>
-            <View style={styles.row}>
-                <SquareWithImageBackground
-                    id="Projects"
-                    title="Projects"
-                    imageUrl={require('../assets/splash.png')}
-                    color="#E5B9FA"
-                    fontSize={45}
-                />
-                <SquareWithImageBackground
-                    id="Experience"
-                    title="Work Experience"
-                    imageUrl={require('../assets/splash.png')}
-                    color="#E5B9FA"
-                    fontSize={45}
-                />
-            </View>
-            <View style={styles.row}>
-                <SquareWithImageBackground
-                    id="Square5"
-                    title="Square 5"
-                    imageUrl={require('../assets/splash.png')}
-                    color="#E5B9FA"
-                    fontSize={45}
-                />
-                <SquareWithImageBackground
-                    id="Square6"
-                    title="Square 6"
-                    imageUrl={require('../assets/splash.png')}
-                    color="#E5B9FA"
-                    fontSize={45}
-                />
-            </View>
-            {/* StatusBar component should be placed outside the main container */}
-            <StatusBar style="auto" />
+            <MenuDropDown />
         </View>
     );
 };
@@ -66,18 +14,6 @@ const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 16,
-    },
-    content: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    row: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between', 
-        marginBottom: 6, 
     },
 });
 
---
+++ Components/MenuDropDown.js
@@ -0,0 +1,45 @@
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import { Menu, MenuProvider, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
+
+
+const MenuDropDown = () => {
+    const navigation = useNavigation();
+    return (
+        <MenuProvider>
+            <View style={styles.container}>
+                <Menu>
+                    <MenuTrigger>
+                        <Icon name="menu" size={40} color="black" style={styles.menuIcon} />
+                    </MenuTrigger>
+                    <MenuOptions style={styles.menuOptions}>
+                        <MenuOption onSelect={() => alert('Save')} text="Education"/>
+                        <MenuOption onSelect={() => alert('Save')} text="Experience"/>
+                        <MenuOption onSelect={() => alert('Save')} text="Projects"/>
+                    </MenuOptions>
+                </Menu>
+            </View>
+        </MenuProvider>
+    );
+};
+
+export default MenuDropDown;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+    },
+    menuIcon: {
+        margin: 10, 
+    },
+    menuOptions: {
+        padding: 5,
+        flexDirection: 'column',
+        alignItems: 'left'
+    },
+});
