f4b5b082c3d92eb95977ef7d68bd54be06bbf9b9
rambaldeo <77577818+rambaldeo@users.noreply.github.com>
2024-04-22 19:24:00 -0400

    Latest Code

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ export default function App() {
           headerTitleStyle: { fontWeight: 'bold'},
           headerTintColor: '#fff'
           }}/>
+          
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
