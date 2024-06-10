364791ba27c3633c12654d941be0c0c842518b0d
rambaldeo <77577818+rambaldeo@users.noreply.github.com>
2024-06-09 20:30:05 -0400

    Images for the Cards

--- Components/Education.js
+++ Components/Education.js
@@ -1,52 +1,63 @@
-import React from "react"; 
-import { StyleSheet, TouchableOpacity, View, Text, Image, ScrollView, Alert } from "react-native";
+import React, {useState, useRef} from "react";
+import { StyleSheet, TouchableOpacity, View, Text, Image, ScrollView, Alert, Animated, Dimensions } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 
+const educationData = [
+    { type: 'image', source: require('../assets/IMG_0008.jpg') },
+    { type: 'image', source: require('../assets/IMG_0006.jpg') },
+    { type: 'image', source: require('../assets/IMG_0009.jpg') },
+    { type: 'image', source: require('../assets/IMG_0010.jpg') },
+    { type: 'image', source: require('../assets/IMG_0011.jpg') },
+];
+    {/* { type: 'text', title: "First Year", description: "This section will be for classes and so on" }, */}
+
 const Education = () => {
+    const [isAnimatedViewVisible, setIsAnimatedViewVisible] = useState(false)
+    const [animatedViewContent, setANimatedViewContent] = useState('')
+    const slideAnim = useRef(new Animated.Value(Dimensions.get('window').height))
+
     const handleCardPress = (message) => {
         Alert.alert(
             message,
             `You clicked the card for ${message}`,
-            [{ text: 'Click to Close'}]
+            [{ text: 'Click to Close' }]
         );
-    }
+    };
 
-    return(
+    const closeAnimatedView = () => {
+        
+    }
+    return (
         <LinearGradient colors={['#2A4EDD', '#4ADEDE', '#2A4EDD']} style={styles.gradient}>
             <ScrollView contentContainerStyle={styles.scrollViewContent}>
                 <TouchableOpacity>
                     <View style={styles.container}>
-                        <Image style={styles.enlargedProfilePicture} resizeMode="contain" source={require('../assets/ontariotechu-og-image.jpg')}/>
+                        <Image style={styles.enlargedProfilePicture} resizeMode="contain" source={require('../assets/ontariotechu-og-image.jpg')} />
                     </View>
                 </TouchableOpacity>
-                <Text>
-                    This page will showcase what I have done during my program at Ontario Tech University starting from first year to graduation
-                </Text>
+
                 <View style={styles.yearContainer}>
                     {educationData.map((item, index) => (
                         <TouchableOpacity key={index} onPress={() => handleCardPress(item.title)}>
-                            <YearCardsStructure title={item.title} description={item.description} index={index}/>
+                            <YearCardsStructure index={index} item={item} />
                         </TouchableOpacity>
                     ))}
-                </View>     
+                </View>
+
+
+
             </ScrollView>
         </LinearGradient>
     );
 };
 
-const YearCardsStructure = ({ title, description, index }) => {
-    return(
-        <View style={[styles.card, index % 2 === 0 ? styles.cardEven : styles.cardOdd]}>
-            <View style={[styles.contentSide, index % 2 === 0 ? styles.contentSideEven : styles.contentSideOdd]}>
-                <Text>{description}</Text>
-            </View>
-            <View style={[styles.picSide, index % 2 === 0 ? styles.picSideEven : styles.picSideOdd]}>
-                
-                <Text>This is for {title}</Text>
-            </View>
+const YearCardsStructure = ({ index, item }) => {
+    return (
+        <View style={[styles.card]}>
+            <Image style={styles.enlargedProfilePicture} resizeMode="contain" source={item.source} />
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     gradient: {
@@ -58,16 +69,17 @@ const styles = StyleSheet.create({
     enlargedProfilePicture: {
         width: '90%',
         height: '90%',
+        borderRadius: 25,
+        overflow: 'hidden'
     },
     container: {
         width: '100%',
         overflow: 'hidden',
-        aspectRatio: 9/5, 
+        aspectRatio: 9 / 5,
         alignItems: 'center',
         marginTop: 35,
     },
     card: {
-        flexDirection: 'row',
         backgroundColor: 'white',
         width: '100%',
         height: 120,
@@ -79,35 +91,12 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         paddingHorizontal: 10,
     },
-    cardEven: {
-        flexDirection: 'row-reverse', // Render contentSide and picSide in reverse order
-    },
-    cardOdd: {
-        flexDirection: 'row', // Render contentSide and picSide in original order
-    },
-    picSide: {
-        borderRadius: 10,
-        flex: 1,
-        backgroundColor: '#4ADEDE',
-        margin: 5
-    },
-    picSideEven: {
-        backgroundColor: 'darkorange',
-    },
-    picSideOdd: {
-        backgroundColor: '#4ADEDE',
-    },
     contentSide: {
         borderRadius: 10,
         flex: 2,
         backgroundColor: 'darkorange',
-        margin: 5
-    },
-    contentSideEven: {
-        backgroundColor: '#4ADEDE',
-    },
-    contentSideOdd: {
-        backgroundColor: 'darkorange',
+        margin: 5,
+        
     },
     yearContainer: {
         width: '90%',
@@ -115,13 +104,5 @@ const styles = StyleSheet.create({
     },
 });
 
-
-const educationData = [
-    { title: "Time on campus", description: "This will be about my time on campus" },
-    { title: "First Year", description: "This section will be for classes and so on" },
-    { title: "Second Year", description: "This section will be for classes and so on" },
-    { title: "Third Year", description: "This section will be for classes and so on" },
-    { title: "Fourth Year", description: "This section will be for classes and so on" },
-];
-
 export default Education;
+
